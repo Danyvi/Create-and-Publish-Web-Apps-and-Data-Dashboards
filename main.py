1f0dc95124a3b18a380e408b49416773db0ff360
Danyvi <30378170+Danyvi@users.noreply.github.com>
2021-12-24 00:41:53 +0000

    add table

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+   'Series_1': [1,3,4,5,7],
+   'Series_2': [10,30,40,100,250]
+}
+
+#create a dataframe from data
+df = pd.DataFrame(data )
 
 st.title('Our first Streamlit App')
 st.subheader('Introducing Streamlit in Automate Everything with Python')
 st.write('''This is our first Web App''')
- 
+
+st.write(df)
